54eb21cb7130aed9b10929f5dc9c64b4ec21b350
유재혁 <yjk7454@naver.com>
2026-04-28 09:06:13 +0900

    feat: `Final02_BestTour - 아이템 좋아요, 삭제 기능 구현 및 적용

--- Final02_BestTour/src/components/BestItem.svelte
+++ Final02_BestTour/src/components/BestItem.svelte
@@ -1,24 +1,25 @@
 <script>
+  import { bests } from "../store"
   import Icon from "@iconify/svelte"
   export let best
   const { id, name, price, descript, image, like } = best
-
+  const { onToggle, onRemove } = bests
 </script>
 <li>
   <a href="#!">
-    <img src={image} alt={name}>
-    <button class="likebox">
+    <img src={image} alt={name} >
+    <button class="likebox" on:click={() => onToggle(id)}>
       {#if like}
-         <Icon icon="tdesign:heart-filled" />
-         {:else}
-         <Icon icon="tdesign:heart" />
+        <Icon icon="tdesign:heart-filled" />
+      {:else}
+        <Icon icon="tdesign:heart" />
       {/if}
     </button>
     <div class="box">
       <h4>{name}</h4>
       <h5>{price.toString().replace(/\B(?=(\d{3})*(?!\d))/g,",")}</h5>
       <p>{descript}</p>
-      <button><Icon icon="gridicons:trash" /></button>
+      <button on:click={() => onRemove(id)}><Icon icon="gridicons:trash" /></button>
     </div>
   </a>
 </li>
--- Final02_BestTour/src/store.js
+++ Final02_BestTour/src/store.js
@@ -6,8 +6,29 @@ import { v4 as uuidv4 } from 'uuid'
 /* 커스텀 스토어 */
 const setBestData = () => {
   const { subscribe, update } = writable(initialBests);
+  
+  const onToggle = (id) => {
+    update(datas => {
+      const setDatas = datas.map(best => {
+        return best.id === id ? { ...best, like: !best.like } : best;
+      })
+      datas = setDatas;
+      return datas;
+    })
+  }
+  const onRemove = (id) => {
+    update(datas => {
+      const setDatas = datas.filter(best => {
+        return best.id !== id;
+      })
+      datas = setDatas;
+      return datas;
+    })
+  }
   return {
-    subscribe
+    subscribe,
+    onToggle,
+    onRemove
   }
 }
 
