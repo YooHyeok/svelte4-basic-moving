ff30492e84fc08ff0d01fbf26372233e48cf6746
유재혁 <yjk7454@naver.com>
2026-05-14 09:11:55 +0900

    Updates

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
-console.log(window.location)
-const HOSTNAME = window.location.hostname
 const CONFIG = {
-  BASE_URL: ['localhost', '127.0.0.1'].includes(HOSTNAME) ? '' : '/svelte4-basic-moving'
+  BASE_URL: ['localhost', '127.0.0.1'].includes(window.location.hostname) 
+              ? '' 
+              : '/svelte4-basic-moving'
 }
